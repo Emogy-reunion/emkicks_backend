c27148d6f87fbdb60839637b10f804d2bacc1464
Emogy-reunion <mv7786986@gmail.com>
2025-08-12 10:45:34 +0300

    add: initialize the application with the configuration settings, this ensures that the app can access them

--- core/__init__.py
+++ core/__init__.py
@@ -1,13 +1,17 @@
 '''
 create the application instance
+initialize the app with the configuration settings
 '''
 from flask import Flask
+from .config import Config
 
 def create_app():
     '''
     creates the application instance and returns it
     '''
     app = Flask(__name__)
+    app.config.from_object(Config)
+
     return app
 
 
