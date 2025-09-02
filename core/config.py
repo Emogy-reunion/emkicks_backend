45dab8f2b0983e071aa576becc70a2215b2c0069
Emogy-reunion <mv7786986@gmail.com>
2025-09-02 10:51:00 +0300

    fix: add brackets when declaring the class

--- core/config.py
+++ core/config.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class Config:
+class Config():
     SECRET_KEY=os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URI')
